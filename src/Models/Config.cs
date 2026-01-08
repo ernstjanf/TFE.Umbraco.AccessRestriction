22a0fbf075feece962656c4c80d2101c50f38258
Ernst Jan Feenstra <ernstjanfeenstra@tres.nl>
2026-01-08 07:28:44 +0100

    IEnumerable doesn't work on production, set it to string[]. Up version to 17.0.3-rc.5

--- src/Models/Config.cs
+++ src/Models/Config.cs
@@ -6,9 +6,9 @@ public class Config
 
     public bool LogBlockedIP { get; set; }
 
-    public IEnumerable<string>? ExcludePaths { get; set; }
+    public string[]? ExcludePaths { get; set; }
 
-    public IEnumerable<string>? IncludePaths { get; set; }
+    public string[]? IncludePaths { get; set; }
 
     public string? LocalHost { get; set; }
 
@@ -16,9 +16,9 @@ public class Config
 
     public string? CustomHeader { get; set; }
 
-    public IEnumerable<string>? Whitelist { get; set; }
+    public string[]? Whitelist { get; set; }
 
-    public IEnumerable<string>? Blacklist { get; set; }
+    public string[]? Blacklist { get; set; }
     
     public int? HttpStatusCode { get; set; }
 }
